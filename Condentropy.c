08c8c8f910f2b76e1c7374d70e16701b6c9e06f9
arman18 <43715453+arman18@users.noreply.github.com>
2019-03-14 08:47:56 +0600

    SPL-1 final

---
+++ Condentropy.c
@@ -0,0 +1,55 @@
+
+#include "miinclude.h"
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+  if(nrhs != 2 && nrhs!=1)
+    mexErrMsgTxt("ERR");
+  if(nlhs > 1)
+    mexErrMsgTxt("Too many output argument <mutual_info>.");
+
+  //check if parameters are correct
+
+  long i,j;
+
+  double *pab = mxGetPr(prhs[0]);
+  long pabhei = mxGetM(prhs[0]);
+  long pabwid = mxGetN(prhs[0]);
+
+  double *pb = mxGetPr(prhs[1]);
+  long pblen =  mxGetM(prhs[1])*mxGetN(prhs[1]);
+
+  if (pblen!=pabwid)
+  {
+    mexErrMsgTxt("Unmatched size: length of the second much be the same as the column of the first.");
+  }
+
+  double **pab2d = new double * [pabwid];
+  for(j=0;j<pabwid;j++)
+    pab2d[j] = pab + (long)j*pabhei;
+
+  //calculate the conditional entropy
+
+  double muInf = 0;
+
+  muInf = 0.0;
+  for (j=0;j<pabwid;j++) // should for pb
+  {
+    for (i=0;i<pabhei;i++) // should for pa
+    {
+      if (pab2d[j][i]!=0 && pb[j]!=0)
+      {
+	muInf += pab2d[j][i] * log(pb[j]/pab2d[j][i]);
+      }
+    }
+  }
+
+  muInf /= log(2.000000);
+
+  plhs[0] = mxCreateDoubleMatrix(1,1, mxREAL);
+  *mxGetPr(plhs[0]) = muInf;
+
+  if(pab2d){delete []pab2d;}
+
+  return;
+}
---
+++ Entropy.c
@@ -0,0 +1,38 @@
+//calculate the entropy of a scalar variable 
+
+
+#include "miinclude.h"
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+  if(nrhs!=1)
+    mexErrMsgTxt("Usage [entropy] = progname(marginprob). .");
+  if(nlhs > 1)
+    mexErrMsgTxt("Too many output argument <entropy>.");
+
+  double *pa = mxGetPr(prhs[0]);
+  long totaln = (long)mxGetM(prhs[0])*mxGetN(prhs[0]);
+
+  double sum = 0.0;
+  double entropy = 0.0;
+  for (long i=0;i<totaln;i++)
+  {
+    double val = pa[i];
+    if (val<0) {printf("Negative Probability!! Wrong data.\n");}
+    sum += val;
+    if (val!=0) {entropy -= val*log(val);}
+  }
+
+  if (sum-1>1e-10)
+  {
+    printf("Dubious data! Sum is not 1.\n");
+  }
+
+  entropy /= log(2.0000);
+
+  plhs[0] = mxCreateDoubleMatrix(1,1, mxREAL);
+  *mxGetPr(plhs[0]) = entropy;
+
+  return;
+}
+
