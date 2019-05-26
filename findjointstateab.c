eadb9b848a957c160a3e9e9ef04059c3d6e1a9d4
Md. Arman Hossain <43715453+arman18@users.noreply.github.com>
2019-05-26 12:00:10 +0600

    Update findjointstateab.c

--- findjointstateab.c
+++ findjointstateab.c
@@ -1,38 +1,99 @@
-#include "miinclude.h"
 
-
-void copyvecdata(double * srcdata, long len, int * desdata, int& nstate,int &minn, int&maxx)
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  if(!srcdata || !desdata)
+
+  if(nrhs != 3 && nrhs !=2 && nrhs!=4)
   {
-    printf("NULL points in copyvecdata()!\n");
-    return;
-  } 
+    mexErrMsgTxt("Err");
+  }
+  if(nlhs > 5)     
+    mexErrMsgTxt("Too many output arguments.");
 
-  long i;
+  long i,j;
 
-  if (srcdata[0]>0)
-    maxx = minn = int(srcdata[0]+0.5);
-  else
-    maxx = minn = int(srcdata[0]-0.5);
+  void *var1 = (void *)mxGetData(prhs[0]);
+  long len1 = mxGetNumberOfElements(prhs[0]);
+  
 
-  int tmp;
-  double tmp1;
-  for (i=0;i<len;i++)
+  void *var2 = (void *)mxGetData(prhs[1]);
+  long len2 = mxGetNumberOfElements(prhs[1]);
+  	
+  if (!var1 || !var2 || !len1 || !len2)
+    mexErrMsgTxt("At least one of the input vectors is invalid.");
+  if (len1!=len2)
+    mexErrMsgTxt("The two vectors/images should have the same length.");
+
+  int b_findstatenum = 1;
+  int nstate1 = 0, nstate2 = 0;
+
+  int * vec1 = new int[len1];
+  int * vec2 = new int[len2];
+  int nrealstate1=0, nrealstate2=0;
+  int minn1,maxx1,minn2,maxx2;  
+    
+  copyvecdata((double *)var1,len1,vec1,nrealstate1,minn1,maxx1);
+
+  copyvecdata((double *)var2,len2,vec2,nrealstate2,minn2,maxx2);
+
+  if (nstate1<nrealstate1)
   {
-    tmp1 = double(srcdata[i]);
-    tmp = (tmp1>0)?(int)(tmp1+0.5):(int)(tmp1-0.5);
-    minn = (minn<tmp)?minn:tmp;
-    maxx = (maxx>tmp)?maxx:tmp;
-    desdata[i] = tmp;
-
+    nstate1 = nrealstate1;
+  }
+  if (nstate2<nrealstate2)
+  {
+    nstate2 = nrealstate2;
   }
 
-  for (i=0;i<len;i++){desdata[i] -= minn;}
-
+  mxArray * m_JointProbab = mxCreateDoubleMatrix(nstate1,nstate2,mxREAL);
+  double *hab = (double *) mxGetPr(m_JointProbab);
+  double **hab2d = new double * [nstate2];
   
-  nstate = (maxx-minn+1);
+  for(j=0;j<nstate2;j++) 
+  {
+    hab2d[j] = hab + (long)j*nstate1;
+  }
+    
+  for (i=0; i<nstate1;i++)
+  {
+    for (j=0; j<nstate2;j++)
+      {
+        hab2d[j][i] = 0;
+      }
+  }
+  
+  
+  for (i=0;i<len1;i++)
+  {
+    hab2d[vec2[i]][vec1[i]] += 1;
+  }
+  
+  
+  for (i=0; i<nstate1;i++)
+  {
+    for (j=0; j<nstate2;j++)
+    {
+      hab2d[j][i] /= len1;
+    }
+     
+  }
+    
 
-  return;
-}
+  plhs[0] = mxCreateDoubleMatrix(len1,1,mxREAL);
+  double * jslist = (double *)mxGetPr(plhs[0]);
+  
+  long * tmphab = new long [long(nstate1)*nstate2];
+  long nmaxstate = 0;
+  for (i=0;i<(long)nstate1*nstate2;i++)
+  {
+    if (hab[i]!=0) 
+    {
+        nmaxstate++;
+        tmphab[i]=nmaxstate;
+    }
+    else tmphab[i]=-1;
+  }
 
+  for (i=0;i<len1;i++)
+  {
+    jslist[i] = tmphab[vec2[i]*nstate1+vec1[i]];
+  }
